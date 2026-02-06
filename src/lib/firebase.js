bb84bd9219e3ac61a120b136be20aa237dcae1d7
MadonnaAdel <madonnadel0@gmail.com>
2026-02-06 21:55:41 +0200

    add .env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,14 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmfuv9q_z2mI3m4mRkBdypBInQa_z4u0A",
-  authDomain: "chat-app-d6a7d.firebaseapp.com",
-  projectId: "chat-app-d6a7d",
-  storageBucket: "chat-app-d6a7d.appspot.com",
-  messagingSenderId: "683973572139",
-  appId: "1:683973572139:web:bc3a6184e5416cce39dc6e",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
